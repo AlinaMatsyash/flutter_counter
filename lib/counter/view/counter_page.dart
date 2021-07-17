23cbf72c2ce316b97e2b6beb8df1a7d0af5c8cad
AlinaMatsyash <alina.mathyahs@gmail.com>
2021-07-17 11:43:45 +0300

    finished

--- lib/counter/view/counter_page.dart
+++ lib/counter/view/counter_page.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_counter/counter/counter.dart';
 import 'counter_view.dart';
-import 'dart:html';
+
 
 class CounterPage extends StatelessWidget {
   const CounterPage({Key? key}) : super(key: key);
 
-  @override //jh
+  @override
   Widget build(BuildContext context) {
     return BlocProvider(create: (_) => CounterBloc(), child: CounterView(),);
   }
